acc3efb06283ded0d1204f7043331294b9005243
jooyyy <joy060102@gmail.com>
2022-09-14 10:55:16 +0800

    remove routing-api

--- packages/routing-api/lib/handlers/router-entities/static-gas-price-provider.ts
+++
@@ -1,9 +0,0 @@
-import { GasPrice, IGasPriceProvider } from '@teleswap/smart-order-router'
-import { BigNumber } from 'ethers'
-
-export class StaticGasPriceProvider implements IGasPriceProvider {
-  constructor(private gasPriceWei: BigNumber) {}
-  async getGasPrice(): Promise<GasPrice> {
-    return { gasPriceWei: this.gasPriceWei }
-  }
-}
