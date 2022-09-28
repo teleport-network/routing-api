08f4d5344a4f6c2abaa5e83f3269313010ba54eb
jooyyy <joy060102@gmail.com>
2022-09-28 10:14:13 +0800

    add change allow headers

--- packages/api/index.ts
+++ packages/api/index.ts
@@ -11,7 +11,7 @@ const PORT = process.env.PORT;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, OriginType");
   res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
   next();
 });
